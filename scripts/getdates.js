68bfd1b835177965ded7d750c0365fba934f3cb9
Shugar-ban <adeoyeayodeji913@gmail.com>
2025-03-09 13:26:26 -0400

    first commit

---
+++ scripts/getdates.js
@@ -0,0 +1,13 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+const designerName = "Adeoye Ayodeji Emmanuel";
+const designerCountry = "Nigeria";
+const lastModifiedDate = document.lastModified;
+
+// Select the first paragraph in the footer
+const footerParagraph = document.querySelector("footer p:first-of-type");
+const secondParagraph = document.querySelector("footer p:nth-of-type(2)");
+
+// Set the content dynamically
+footerParagraph.innerHTML = `&copy; ${currentYear}  ${designerName} , ${designerCountry}`
+secondParagraph.textContent = `This document was last modified on: ${lastModifiedDate}`;
